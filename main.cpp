c5e1f340bf0ccc4bf6f5ff79fe097ccaf74dcf93
mou <mouna.erraji@stud.unifi.it>
2023-12-22 17:54:22 +0100

    Prove di aggiunta azioni e utenti banca

--- main.cpp
+++ main.cpp
@@ -14,14 +14,14 @@ int main() {
 
         Bank bank;
         Account u0("mouna", "erraji", 0, 1000);
-        //Account  u1(1, 100);
+        Account  u1(1, 100);
         // Accountu2(2, 200);
         bank.add_account(u0);
-        //bank.add_account(u1);
+        bank.add_account(u1);
         // bank.add_account(u2);
         bank.account_deposita(0, 100);
         bank.account_preleva(0, 100);
         bank.account_preleva(0,300);
-        //bank.account_deposita(0,300);
+        bank.account_deposita(0,300);
         return 0;
 }
